670c58fce8180c158fa5fb55c737f6df59d20f70
Inaolaji Ridwan <ridwaninaolaji@gmail.com>
2024-02-19 09:42:35 +0100

    adding my CV

--- main.js
+++ main.js
@@ -1,51 +1,43 @@
+var sunDisplay = document.getElementById("sun");
+var moonDisplay = document.getElementById("moon");
 
+sunDisplay.style.display = "none";
 
-var sunDisplay= document.getElementById('sun');
-var moonDisplay = document.getElementById('moon');
+moonDisplay.onclick = function () {
+  moonDisplay.style.display = "none";
+  document.getElementById("body").style.backgroundColor = "darkseagreen";
+  sunDisplay.style.display = "block";
+  document.getElementById("body").style.color = "black";
+  document.querySelector(".navbar-brand").style.color = "black";
 
-sunDisplay.style.display="none";
+  document.getElementById("jj").style.color = "black";
+  document.getElementById("jj1").style.color = "black";
+  document.getElementById("jj2").style.color = "black";
+  document.getElementById("jj3").style.color = "black";
 
+  sunDisplay.onclick = function () {
+    moonDisplay.style.display = "block";
+    document.getElementById("body").style.backgroundColor = "darkgray";
+    document.getElementById("logoA").style.color = "black";
+    sunDisplay.style.display = "none";
+    document.getElementById("body").style.color = "black";
+  };
+};
 
-moonDisplay.onclick = function() {
-  moonDisplay.style.display='none';
-  document.getElementById('body').style.backgroundColor="black";
-  sunDisplay.style.display="block";
-  document.getElementById('body').style.color="white";
-  document.querySelector(".navbar-brand").style.color="black";
-
-  document.getElementById("jj").style.color="black";
-  document.getElementById("jj1").style.color="black";
-  document.getElementById("jj2").style.color="black";
-  document.getElementById("jj3").style.color="black";
-  
-
-  sunDisplay.onclick = function() {
-    moonDisplay.style.display='block';
-    document.getElementById('body').style.backgroundColor="white";
-    document.getElementById("logoA").style.color="black"
-    sunDisplay.style.display="none";
-    document.getElementById('body').style.color="black";
-    
-  }
-
-  
-
-  
-}
-
-const typed = new Typed('.typer', {
-  strings: ['Inaolaji Ridwan.' ],
+const typed = new Typed(".typer", {
+  strings: ["Inaolaji Ridwan."],
   typeSpeed: 500,
   backspeed: 500,
-  loop: true
+  loop: true,
 });
 
-
-document.addEventListener("click", function(e) {
-    if (e.target.classList.contains("gallery-item")){
-        const src=e.target.getAttribute("src");
-        document.querySelector(".modal-img").src=src;
-        const myModal = new bootstrap.Modal(document.getElementById("gallery-modal"));
-        myModal.show();
-    }
-})
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("gallery-item")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("gallery-modal")
+    );
+    myModal.show();
+  }
+});
